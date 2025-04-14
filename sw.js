3e6bf3b740435634ad97c160d7d712393f743e9b
nani0715 <nancy22377856@gmail.com>
2025-04-14 14:54:46 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener("install", e => {
-  console.log("[ServiceWorker] Installed");
-});
-self.addEventListener("fetch", e => {
-  console.log("[ServiceWorker] Fetching", e.request.url);
-});
