290dba919e7015299cf4c45624fe2d70c21fe4f2
nani0715 <nancy22377856@gmail.com>
2025-04-14 12:00:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-self.addEventListener('install', () => {});
+self.addEventListener("install", e => {
+  console.log("[ServiceWorker] Installed");
+});
+self.addEventListener("fetch", e => {
+  console.log("[ServiceWorker] Fetching", e.request.url);
+});
