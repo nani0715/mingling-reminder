9bb84c98ca409080a51333202a95fb015a8dbefa
nani0715 <nancy22377856@gmail.com>
2025-04-14 11:53:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-self.addEventListener("install", function(e) {
-    e.waitUntil(
-        caches.open("mingling-reminder").then(function(cache) {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/style.css",
-                "/logic.js",
-                "/quotes.json",
-                "/manifest.json",
-                "/icon.png"
-            ]);
-        })
-    );
-});
+self.addEventListener('install', () => {});
